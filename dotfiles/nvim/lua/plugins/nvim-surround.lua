7111b59ec60abd79f573fd10d411ce0fba1df236
Tyler Anderton <tanderton@tractian.com>
2026-03-26 15:25:31 -0600

    fix: migrated nvim-surround options from v3 to v4

--- dotfiles/nvim/lua/plugins/nvim-surround.lua
+++ dotfiles/nvim/lua/plugins/nvim-surround.lua
@@ -1,11 +1,18 @@
+-- return {
+--   "kylechui/nvim-surround",
+--   opts = {
+--     keymaps = {
+--       normal = false,
+--       normal_cur = false,
+--       normal_line = false,
+--       visual = "S",
+--     },
+--   },
+-- }
+-- migration from v3 to v4 removed keymaps dict
 return {
   "kylechui/nvim-surround",
-  opts = {
-    keymaps = {
-      normal = false,
-      normal_cur = false,
-      normal_line = false,
-      visual = "S",
-    },
-  },
+  init = function()
+    vim.g.nvim_surround_no_normal_mappings = true
+  end,
 }
